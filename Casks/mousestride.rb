d3ed2de77ce75d74d0265f2beb7f077184ad1269
jenicek <87394351+trustbe@users.noreply.github.com>
2026-02-16 23:29:52 +0100

    chore: update cask to v0.0.9

--- Casks/mousestride.rb
+++ Casks/mousestride.rb
@@ -1,11 +1,11 @@
 cask "mousestride" do
-  version "0.0.4"
-  sha256 "e41eb24f1b67a3cc62887a9ffb524970c7d54cba22593e471df95531074e9a83"
+  version "0.0.9"
+  sha256 "25766dd360c12d78766711ad84628268df9370b5e9c870444c53e41b4e5f2fdf"
 
   url "https://github.com/trustbe/MouseStride/releases/download/v#{version}/MouseStrideDaemon-v#{version}.zip"
   name "MouseStride"
   desc "Lightweight mouse distance tracker for macOS"
-  homepage "https://trustbe.github.io/MouseStride/"
+  homepage "https://mousestride.trustbe.com/"
 
   depends_on macos: ">= :ventura"
 
